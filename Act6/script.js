76378ddb9359c513bff2e518bdb7bed5834a921b
0xnamelessDev <javierollersanchez@gmail.com>
2024-01-26 11:23:48 +0100

    Act6 y Act7 mod

--- Act6/script.js
+++ Act6/script.js
@@ -1,4 +1,4 @@
 function resultado(){
     document.getElementById("result").innerHTML = 
-    parseInt(document.getElementById("num").value) % 2 == 0? "El número es par":"El número es impar";
+        parseInt(document.getElementById("num").value) % 2 == 0? "El número es par":"El número es impar";
 }
